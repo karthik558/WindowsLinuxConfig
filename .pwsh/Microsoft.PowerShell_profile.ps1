0d62532668bc099a07f9f34db43bea72ab46d58a
KARTHIK LAL <karthik.lal558@gmail.com>
2022-12-01 17:30:04 +0530

    dotfiles: powershell-config: import git_Aliases to the config_file
    * Also refactor by 1/4
    
    Signed-off-by: KARTHIK LAL <karthik.lal558@gmail.com>

--- .pwsh/Microsoft.PowerShell_profile.ps1
+++ .pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,29 @@
-cls #clear the runtime_preview on the opening
+cls #clear the terminal on the opening
+
+# Some git aliases
+
+function Get-GitStatus { & git status -sb $args }
+New-Alias -Name gs -Value Get-GitStatus -Force -Option AllScope
+function Get-GitCommit { & git commit -s $args }
+New-Alias -Name gc -Value Get-GitCommit -Force -Option AllScope
+function Get-GitAdd { & git add --all $args }
+New-Alias -Name ga -Value Get-GitAdd -Force -Option AllScope
+function Get-GitTree { & git log --graph --oneline --decorate $args }
+New-Alias -Name gt -Value Get-GitTree -Force -Option AllScope
+function Get-GitPush { & git push $args }
+New-Alias -Name gps -Value Get-GitPush -Force -Option AllScope
+function Get-GitPull { & git pull $args }
+New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name gf -Value Get-GitFetch -Force -Option AllScope
+function Get-GitCheckout { & git checkout $args }
+New-Alias -Name gco -Value Get-GitCheckout -Force -Option AllScope
+function Get-GitBranch { & git branch $args }
+New-Alias -Name gb -Value Get-GitBranch -Force -Option AllScope
+function Get-GitRemote { & git remote -v $args }
+New-Alias -Name gr -Value Get-GitRemote -Force -Option AllScope
+function Get-GitDiff { & git diff $args }
+New-Alias -Name gd -Value Get-GitDiff -Force -Option AllScope
 
 New-Alias k kubectl
 Remove-Alias h
